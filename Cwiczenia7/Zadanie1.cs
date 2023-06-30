99a18a50c6030c2cdf0a02fbf30ded6e624b31a7
miparuzel <128227022+miparuzel@users.noreply.github.com>
2023-06-30 23:09:33 +0200

    Add files via upload

---
+++ Cwiczenia7/Zadanie1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public enum Day
+{
+    Poniedzialek = 1,
+    Wtorek = 2,
+    Sroda = 3,
+    Czwartek = 4,
+    Piatek = 5,
+    Sobota = 6,
+    Niedziela = 7
+}
+
+public enum washing
+{
+    Pranie,
+    Plukanie,
+    Wirowanie,
+    Suszenie
+}
+
+public enum meals
+{
+    Śniadanie = 1,
+    Obiad = 2,
+    Podwieczorek = 3,
+    Kolacja = 4
+}
+public enum chess
+{
+    Pion,
+    Wieza,
+    Skoczek,
+    Goniec,
+    Hetman,
+    Krol
+}
---
+++ Cwiczenia7/Zadanie2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct student
+{
+    public string name;
+    public string surname;
+    public string major;
+    public int studentid;
+}
+
+struct subject
+{
+    public string subject_name;
+    public int subject_id;
+    public string teacher;
+    public string faculty;
+}
+
+struct teacher
+{
+    public string name;
+    public string surname;
+    public string major;
+    public string faculty;
+    public int teacherid;
+}
